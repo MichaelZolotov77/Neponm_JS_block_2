da86170d8ed8e0f0f22f28327f721a909454c57b
MichaelZolotov77 <michaelzol@yandex.ru>
2023-12-25 18:51:21 +0300

    Угодай число, мой вариант

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-const numbers = [1, 2, 2, 13, 11, 7, 20, 22, 14, 15];
-
-function mainCode(numbers) {
-  const evenNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      evenNumbers.push(numbers[i]);
-      if (evenNumbers.length === 3) break;
-    }
-  }
-
-  return evenNumbers;
+function milesToKm(miles) {
+  return miles * 1.609;
 }
 
-console.log(mainCode(numbers));
+console.log(milesToKm(2));
